8f02d4a4ceb77cb3f7bed76d1451e521fbbf3368
ryubears <ryubears@gmail.com>
2018-02-13 16:47:42 -0600

    Add Week Three Spring 2018

---
+++ S18 Week Three/MyWeatherStartThree/app/src/main/java/com/yehyunryu/android/myweatherstartthree/MainActivity.java
@@ -0,0 +1,23 @@
+package com.yehyunryu.android.myweatherstartthree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TODO 3. Add Weather Class
+        //TODO 4. Create Fake Data
+        //TODO 5. Instantiate RecyclerView
+        //TODO 6. Create Adapter Class
+        //TODO 7. Create ViewHolder Class
+        //TODO 8. Create Methods and Constructors
+        //TODO 9. Instantiate Adapter and LayoutManager
+        //TODO 10. Attach Adapter and LayoutManager to RecyclerView
+
+    }
+}
